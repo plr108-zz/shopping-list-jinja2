d6f485aa111c3fabd37241ca204089a101667d44
Patrick Roche <patrick.l.roche@gmail.com>
2017-05-07 16:23:13 -0400

    Initial commit.  Contains "Hello world" version of project.

---
+++ templates.py
@@ -0,0 +1,18 @@
+import os
+
+import webapp2
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+
+class MainPage(Handler):
+    def get(self):
+        self.write("WHAZZUP")
+
+
+app = webapp2.WSGIApplication([('/', MainPage),
+                               ],
+                              debug=True)
